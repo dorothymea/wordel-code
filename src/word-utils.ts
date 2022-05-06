1d01efdcf6682bc76205e16428cf26581f190d49
dorothymea <meao0505@gmail.com>
2022-05-07 01:40:57 +0800

    判断letter状态

--- src/word-utils.ts
+++ src/word-utils.ts
@@ -1,6 +1,31 @@
 import wordBank from './word-bank.json'
 
-export default function getRandomWord():string{
+export enum LetterState{
+    Miss,
+    Present,
+    Match
+}
+
+function getRandomWord():string{
     return wordBank[Math.floor(Math.random() * wordBank.length)]
 }
 
+function computeGuess(guess:string,answer:string):LetterState[]{
+    const result:LetterState[] = []
+    const guessArray = guess.split('')
+    const answerArray = answer.split('')
+
+    guessArray.forEach((letter,index) =>{
+        if(letter === answerArray[index]){
+            result.push(LetterState.Match)
+        }else if(answerArray.includes(letter)){
+            result.push(LetterState.Present)
+        }else {
+            result.push(LetterState.Miss)
+        }
+    })
+
+    return result
+}
+
+export {getRandomWord, computeGuess}
