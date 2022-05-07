18447d10d91d65d4ab1d2794882f858dc27a0e61
dorothymea <meao0505@gmail.com>
2022-05-07 10:13:45 +0800

    添加检查单词函数并测试

---
+++ src/word-utils.test.ts
@@ -0,0 +1,55 @@
+
+import { describe, expect, test } from 'vitest';
+import { computeGuess, LetterState } from './word-utils';
+
+describe('computeGuess', () => {
+    test('works with match and presents', () => {
+        expect(computeGuess('boost', 'basic')).toEqual([
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Present,
+            LetterState.Miss,
+        ]);
+    });
+
+    test('full match', () => {
+        expect(computeGuess('boost', 'boost')).toEqual([
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match,
+        ]);
+    });
+
+    test('full miss', () => {
+        expect(computeGuess('guard', 'boost')).toEqual([
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+        ]);
+    });
+
+    test('only does one match when two letters exist', () => {
+        expect(computeGuess('solid', 'boost')).toEqual([
+            LetterState.Present,
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+        ]);
+    });
+
+    test('only does one match when two letters exist', () => {
+        expect(computeGuess('exist', 'enter')).toEqual([
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Present,
+        ]);
+    });
+});
--- src/word-utils.ts
+++ src/word-utils.ts
@@ -12,11 +12,19 @@ function getRandomWord():string{
 
 function computeGuess(guess:string,answer:string):LetterState[]{
     const result:LetterState[] = []
+
+    if(guess.length !== answer.length){return result}
+
     const guessArray = guess.split('')
     const answerArray = answer.split('')
+    const answerLetterCount:Record<string, number> = {}
 
     guessArray.forEach((letter,index) =>{
-        if(letter === answerArray[index]){
+        const currentAnswerLetter = answerArray[index]
+
+        answerLetterCount[currentAnswerLetter]= answerLetterCount[currentAnswerLetter]?answerLetterCount[currentAnswerLetter]+1:1
+
+        if(currentAnswerLetter === letter){
             result.push(LetterState.Match)
         }else if(answerArray.includes(letter)){
             result.push(LetterState.Present)
@@ -25,7 +33,37 @@ function computeGuess(guess:string,answer:string):LetterState[]{
         }
     })
 
+    result.forEach((curResult,resultIndex) =>{
+        if(curResult !== LetterState.Present){return}
+        const guessLetter = guessArray[resultIndex]
+
+        answerArray.forEach((curAnswer,ansIndex)=>{
+            if(curAnswer !== guessLetter){ return }
+            if(result[ansIndex] === LetterState.Match){
+                result[resultIndex] = LetterState.Miss
+            }
+            if(answerLetterCount[guessLetter]<=0){
+                result[resultIndex] = LetterState.Miss
+            }
+        })
+    })
+
     return result
 }
 
 export {getRandomWord, computeGuess}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
